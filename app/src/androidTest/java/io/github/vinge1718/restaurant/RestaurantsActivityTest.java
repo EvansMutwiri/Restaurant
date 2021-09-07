8ccdf2e6b30710d2b642de27978482b5021bd041
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-09-07 15:48:41 +0300

    List view

---
+++ app/src/androidTest/java/io/github/vinge1718/restaurant/RestaurantsActivityTest.java
@@ -0,0 +1,17 @@
+package io.github.vinge1718.restaurant;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class RestaurantsActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
